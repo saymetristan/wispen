d2b99a5318bffa55017a5dfa0c3408b1c0f7cba5
Jorge Tristan <Jorge-tristan@Josues-MacBook-Air.local>
2024-08-03 20:52:02 -0600

    middleware onboarding/app

---
+++ src/middleware/userStatusMiddleware.js
@@ -0,0 +1,37 @@
+import User from '../models/User.js';
+import logger from '../utils/logger.js';
+
+const userStatusMiddleware = async (req, res, next) => {
+  const userId = req.body.WaId;
+
+  if (!userId) {
+    return next();
+  }
+
+  try {
+    const user = await User.findByPk(userId);
+
+    if (!user) {
+      return next();
+    }
+
+    const perfilCompleto = user.name && user.ocupacion && user.ingresoMensualPromedio && user.limiteGastoMensual && user.monedaPreferencia && (user.ahorrosActuales !== null && user.ahorrosActuales !== undefined);
+
+    if (perfilCompleto && user.assistant_ID === 'asst_AUZqqVPMNJFedXX3A5fYBp7f') {
+      user.assistant_ID = 'asst_4aycqyziNvkiMm88Sf1CvPJg';
+      user.threadId = null;
+      user.isOnboarding = false;
+      user.isNewUser = false;
+      await user.save();
+
+      logger.info(`Usuario ${userId} actualizado: perfil completo, nuevo Assistant ID asignado.`);
+    }
+
+    next();
+  } catch (error) {
+    logger.error('Error en userStatusMiddleware:', error);
+    next(error);
+  }
+};
+
+export default userStatusMiddleware;
