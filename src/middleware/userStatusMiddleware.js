7a2b0dfcc4a44c3ac371e0486de5dd6a06691f5b
Jorge Tristan <Jorge-tristan@Josues-MacBook-Air.local>
2024-08-06 20:59:34 -0600

    bai onboarding

--- src/middleware/userStatusMiddleware.js
+++ src/middleware/userStatusMiddleware.js
@@ -12,43 +12,12 @@ const userStatusMiddleware = async (req, res, next) => {
   try {
     const user = await User.findByPk(userId);
 
-    if (!user) {
-      logger.info(`Usuario no encontrado para el ID: ${userId}`);
-      return next();
-    }
-
-    logger.info(`Estado actual del usuario ${userId}:`, {
-      name: user.name,
-      isNewUser: user.isNewUser,
-      isOnboarding: user.isOnboarding,
-      assistant_ID: user.assistant_ID,
-      threadId: user.threadId
-    });
-
-    const perfilCompleto = user.name && user.ocupacion && user.ingresoMensualPromedio && user.limiteGastoMensual && user.monedaPreferencia && (user.ahorrosActuales !== undefined);
-
-    logger.info(`Perfil completo para el usuario ${userId}: ${perfilCompleto}`);
-
-    if (perfilCompleto && !user.threadId) {
-      user.assistant_ID = 'asst_4aycqyziNvkiMm88Sf1CvPJg';
-      user.isOnboarding = false;
-      user.isNewUser = false;
-
-      // Crear un nuevo thread para el asistente de finanzas
-      const newThread = await openai.beta.threads.create();
-      user.threadId = newThread.id;
-      user.threadCreatedAt = new Date();
-
-      await user.save();
-
-      logger.info(`Usuario ${userId} actualizado: perfil completo, nuevo Assistant ID y Thread ID asignados.`, {
+    if (user) {
+      logger.info(`Estado actual del usuario ${userId}:`, {
+        name: user.name,
         assistant_ID: user.assistant_ID,
-        threadId: user.threadId,
-        isOnboarding: user.isOnboarding,
-        isNewUser: user.isNewUser
+        threadId: user.threadId
       });
-    } else {
-      logger.info(`No se actualizó el usuario ${userId}. Perfil completo: ${perfilCompleto}, Assistant ID actual: ${user.assistant_ID}, ThreadId: ${user.threadId}`);
     }
 
     next();
